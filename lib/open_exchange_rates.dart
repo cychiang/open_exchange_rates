08b3efe52248a2d9eceba446f37c3a831f23348c
cychiang <cychiang0823@gmail.com>
2018-03-09 09:54:56 +0800

    modify access pattern

--- lib/open_exchange_rates.dart
+++ lib/open_exchange_rates.dart
@@ -1,91 +1,47 @@
 import 'package:http/http.dart' as http;
 import 'dart:io' show Platform;
 
-// Copy from api document from openexchangerates.org
-// Parameters
-// - app_id: string(Must)
-// - base: string(Optional)
-//   - Change base currency (3-letter code, default: USD)
-// - symbols: string(Optional)
-//   - Limit results to specific currencies (comma-separated list of 3-letter codes)
-// - show_alternative: boolean(Optional)
-//   - Extend returned values with alternative, black market and digital currency rates
-// - prettyprint:	boolean(Optional)
-//   - Human-readable response for debugging (response size will be much larger)
-
-class OpenExchangeRatesOptions {
-  String base = null;
-  String symbols = null;
-  bool show_alternative = null;
-  bool prettyprint = null;
-  // init options
-  OpenExchangeRatesOptions() {}
-  String get() {
-    return 'query_string';
-  }
-}
-
 // create class to handle requests
 class OpenExchangeRates {
   // init variable
   static const base_path = 'https://openexchangerates.org/api/';
-  OpenExchangeRatesOptions opts;
+  // OpenExchangeRatesOptions opts;
+  String query = null;
   String api_key = null;
-  // create constructer
-  OpenExchangeRates(OpenExchangeRatesOptions opts, String api_key) {
-    this.opts = opts;
-    this.api_key = api_key;
+  // Query options
+  String base = null;
+  String symbols = null;
+  bool show_alternative = null;
+  bool prettyprint = null;
+  // each api has its own init function
+  OpenExchangeRates.initLatest(){
+    // Parameters
+    // - app_id: string(Must)
+    // - base: string(Optional)
+    //   - Change base currency (3-letter code, default: USD)
+    // - symbols: string(Optional)
+    //   - Limit results to specific currencies (comma-separated list of 3-letter codes)
+    // - show_alternative: boolean(Optional)
+    //   - Extend returned values with alternative, black market and digital currency rates
+    // - prettyprint:	boolean(Optional)
+    //   - Human-readable response for debugging (response size will be much larger)
+    // TODO: 
   }
-  // get latest 
-  latest(){
-    // compose query string
-
-    return http.get(
-      base_path + '/latest.json?app_id=' + api_key
-    );
-  }
-  historical(){
-    // 
-    return http.get(
-      '/historical/*.json'
-    );
-  }
-  currencies(){
-    //
-    return http.get(
-      '/currencies.json'
-    );
-  }
-  timeseries(){
-    // 
-    return http.get(
-      '/time-series.json'
-    );
-  }
-  convert(){
-    // 
-    return http.get(
-      '/convert'
-    );
-  }
-  ohlc(){
-    // 
-    return http.get(
-      '/ohlc.json'
-    );
-  }
-  usage(){
-    // 
-    return http.get(
-      '/usage.json'
-    );
-  }
+  OpenExchangeRates.initHistorical(){}
+  OpenExchangeRates.initCurrencies(){}
+  OpenExchangeRates.initTimeseries(){}
+  OpenExchangeRates.initConvert(){}
+  OpenExchangeRates.initOhlc(){}
+  OpenExchangeRates.initUsage(){}
+  get() => http.get(base_path + this.query);
 }
 
 main() async {
   String api_key = Platform.environment['OER_API_KEY'];
-  OpenExchangeRatesOptions opts = new OpenExchangeRatesOptions();
-  var oer_client = new OpenExchangeRates(opts, api_key);
-  var response = await oer_client.latest();
-  print('${response.body}');
+  // How to use
+  // Get query instance at initial time
+  OpenExchangeRates queryLatest = new OpenExchangeRates.initLatest();
+  // Use get function to perform query and get result
+  var latestResponse = await queryLatest.get();
+  print('${latestResponse}');
 }
