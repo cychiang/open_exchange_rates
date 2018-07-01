afc30d11edc7aa78356e17a753d416293a9688cc
cychiang <cychiang0823@gmail.com>
2018-07-01 10:11:41 +0800

    update example

--- example/main.dart
+++ example/main.dart
@@ -2,7 +2,7 @@ import 'package:open_exchange_rates/open_exchange_rates.dart';
 
 // example code
 main() async {
-  String api_key = 'api_key';
+  String api_key = '';
   ExchangerBloc blocLatest = ExchangerBloc.fromLatest(Latest());
   ExchangerBloc blocCurrencies = ExchangerBloc.fromCurrencies(Currencies());
   // Latest
@@ -10,7 +10,7 @@ main() async {
   blocLatest.results.forEach((item) => item.forEach((val) =>
       print('${val.currency}: ${val.ratio}')
   ));
-  // Latest
+  // Currencies
   blocCurrencies.query.add(Params(api_key: api_key));
   blocCurrencies.currency.forEach((item) => item.forEach((val) =>
       print('${val.currency}: ${val.name}')
