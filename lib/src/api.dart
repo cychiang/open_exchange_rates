5f3206f57f3f18ef0995e0a5084d06a74736dadd
cychiang <cychiang0823@gmail.com>
2018-07-04 11:20:10 +0800

    cleanup api struct

--- lib/src/api.dart
+++ lib/src/api.dart
@@ -1,39 +1,36 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'package:uri/uri.dart';
+
+//import 'package:uri/uri.dart';
 import 'rate.dart';
 import 'params.dart';
+import 'api_base.dart';
 
-class APIBase {
-  final http.Client _client = http.Client();
-}
 
 class Historical extends APIBase {
-  final UriTemplate _url = UriTemplate(
-      'https://openexchangerates.org/api/historical/{date}.json?app_id={api_key}'
-  );
-  Future<List<Rate>> get(Params query) async {
+  final String _url = 'https://openexchangerates.org/api/historical/';
+  Future<List<Rate>> get(Params params) async {
     List<Rate> list = [];
     // Format Uri
-    String _compose_url = _url.expand({
-      'api_key': query.api_key,
-      'date': query.date
-    });
-    if (query.base != null) {
-      _compose_url += '?base=${query.base}';
-    }
-    if (query.symbols != null) {
-      _compose_url += '?symbols=${query.symbols}';
-    }
-    if (query.prettyprint != null) {
-      _compose_url += '?symbols=${query.prettyprint}';
-    }
-    if (query.show_alternative != null) {
-      _compose_url += '?symbols=${query.show_alternative}';
-    }
-    await _client
-        .get(_compose_url)
+    String url = _url + init_query(params);
+    await client
+        .get(url)
+        .then((res) => res.body)
+        .then(json.decode)
+        .then((json) => json['rates'])
+        .then((rates) => rates.forEach((key, value)
+    => list.add(Rate.fromMap(key, value))));
+    return list;
+  }
+}
+
+class Latest extends APIBase {
+  final String _url = 'https://openexchangerates.org/api/latest.json';
+  Future<List<Rate>> get(Params params) async {
+    List<Rate> list = [];
+    String url = _url + init_query(params);
+    await client
+        .get(url)
         .then((res) => res.body)
         .then(json.decode)
         .then((json) => json['rates'])
@@ -45,57 +42,15 @@ class Historical extends APIBase {
 
 class Currencies extends APIBase {
   final String _url = 'https://openexchangerates.org/api/currencies.json';
-  Future<List<Currency>> get(Params query) async {
+  Future<List<Currency>> get(Params params) async {
     List<Currency> list = [];
-    // Format Uri
-    String _compose_url = _url;
-    if (query.prettyprint != null) {
-      _compose_url += '?symbols=${query.prettyprint}';
-    }
-    if (query.show_alternative != null) {
-      _compose_url += '?symbols=${query.show_alternative}';
-    }
-    if (query.show_inactive != null) {
-      _compose_url += '?show_inactive=${query.show_inactive}';
-    }
-    await _client
-        .get(_compose_url)
+    String url = _url + init_query(params);
+    await client
+        .get(url)
         .then((res) => res.body)
         .then(json.decode)
         .then((rates) => rates.forEach((key, value)
     => list.add(Currency.fromMap(key, value))));
-    return list;
-  }
-}
-
-class Latest extends APIBase {
-  final UriTemplate _url = UriTemplate(
-      'https://openexchangerates.org/api/latest.json'
-      '?app_id={api_key}'
-  );
-  Future<List<Rate>> get(Params query) async {
-    List<Rate> list = [];
-    // Format Uri
-    String _compose_url = _url.expand({'api_key': query.api_key});
-    if (query.base != null) {
-      _compose_url += '?base=${query.base}';
-    }
-    if (query.symbols != null) {
-      _compose_url += '?symbols=${query.symbols}';
-    }
-    if (query.prettyprint != null) {
-      _compose_url += '?symbols=${query.prettyprint}';
-    }
-    if (query.show_alternative != null) {
-      _compose_url += '?symbols=${query.show_alternative}';
-    }
-    await _client
-        .get(_compose_url)
-        .then((res) => res.body)
-        .then(json.decode)
-        .then((json) => json['rates'])
-        .then((rates) => rates.forEach((key, value)
-    => list.add(Rate.fromMap(key, value))));
     return list;
   }
 }
