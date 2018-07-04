c9a92640d9079fd0f325ca75f61e06ebae94c4ad
cychiang <cychiang0823@gmail.com>
2018-07-04 15:40:27 +0800

    update example code

--- example/main.dart
+++ example/main.dart
@@ -3,34 +3,23 @@ import 'package:open_exchange_rates/oer.dart';
 // example code
 main() async {
   String api_key = 'api_key';
+  // Query Latest
   QueryLatest latest = QueryLatest.get();
-  latest.query.add(Params(
-    api_key: api_key
-  ));
-  latest.results.forEach((item) => item.forEach((val) =>
-      print('${val.currency}: ${val.ratio}')
-  ));
+  latest.query.add(Params(api_key: api_key));
+  latest.results.forEach(
+      (item) => item.forEach((val) => print('${val.currency}: ${val.ratio}')));
   latest.dispose();
+  // Query Currencies
+  QueryCurrencies currencies = QueryCurrencies.get();
+  currencies.query.add(Params(api_key: api_key));
+  currencies.results.forEach(
+      (item) => item.forEach((val) => print('${val.currency}: ${val.name}')));
+  currencies.dispose();
+  // Query Historical
+  QueryHistorical historical = QueryHistorical.get();
+  historical.query.add(Params(api_key: api_key, date: '2018-07-01'));
+  historical.results.forEach(
+      (item) => item.forEach((val) => print('${val.currency}: ${val.ratio}')));
 
-//  ExchangerBloc blocLatest = ExchangerBloc.fromLatest(Latest());
-//  ExchangerBloc blocCurrencies = ExchangerBloc.fromCurrencies(Currencies());
-//  ExchangerBloc blocHistorical = ExchangerBloc.fromHistorical(Historical());
-  // Latest
-//  blocLatest.query.add(Params(api_key: api_key));
-//  blocLatest.results.forEach((item) => item.forEach((val) =>
-//      print('${val.currency}: ${val.ratio}')
-//  ));
-  // Currencies
-//  blocCurrencies.query.add(Params(api_key: api_key));
-//  blocCurrencies.currency.forEach((item) => item.forEach((val) =>
-//      print('${val.currency}: ${val.name}')
-//  ));
-  // Historical
-//  blocHistorical.query.add(Params(api_key: api_key, date: '2018-07-01'));
-//  blocHistorical.history.forEach((item) => item.forEach((val) =>
-//      print('${val.currency}: ${val.ratio}')
-//  ));
-//  blocLatest.dispose();
-//  blocCurrencies.dispose();
-//  blocHistorical.dispose();
+  historical.dispose();
 }
