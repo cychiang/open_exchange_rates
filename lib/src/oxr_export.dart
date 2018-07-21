82175755cf261ca82df4092f07be67a3d798e4f4
cychiang <cychiang0823@gmail.com>
2018-07-21 17:23:54 +0800

    return json object(Map) instead of Class, so delete all data class

--- lib/src/oxr_export.dart
+++ lib/src/oxr_export.dart
@@ -5,34 +5,6 @@ import 'package:open_exchange_rates/src/api/oxr_timeseries.dart';
 import 'package:open_exchange_rates/src/api/oxr_convert.dart';
 import 'package:open_exchange_rates/src/api/oxr_ohlc.dart';
 import 'package:open_exchange_rates/src/api/oxr_usage.dart';
-// Define base units
-
-class Currency {
-  final String _currency_code;
-  final String _currency_name;
-
-  Currency(_currency_code, _currency_name)
-      : _currency_code = _currency_code,
-        _currency_name = _currency_name;
-}
-
-class Rate {
-  final String _currency_code;
-  final num _rate;
-
-  Rate(_currency_code, _rate)
-      : _currency_code = _currency_code,
-        _rate = _rate;
-}
-
-class LatestResponse {
-  final String _base;
-  final List<Rate> _rates;
-
-  LatestResponse(_base, _rates)
-      : _base = _base,
-        _rates = _rates;
-}
 
 class New {
   Latest latest;
