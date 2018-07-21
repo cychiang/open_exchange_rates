876be3b451afd4e15325190a432366fcec583bd0
cychiang <cychiang0823@gmail.com>
2018-07-21 17:09:14 +0800

    add api to export

--- lib/src/oxr_export.dart
+++ lib/src/oxr_export.dart
@@ -1,6 +1,10 @@
-import 'oxr_latest.dart';
-import 'oxr_currencies.dart';
-import 'oxr_historical.dart';
+import 'package:open_exchange_rates/src/api/oxr_latest.dart';
+import 'package:open_exchange_rates/src/api/oxr_currencies.dart';
+import 'package:open_exchange_rates/src/api/oxr_historical.dart';
+import 'package:open_exchange_rates/src/api/oxr_timeseries.dart';
+import 'package:open_exchange_rates/src/api/oxr_convert.dart';
+import 'package:open_exchange_rates/src/api/oxr_ohlc.dart';
+import 'package:open_exchange_rates/src/api/oxr_usage.dart';
 // Define base units
 
 class Currency {
@@ -34,9 +38,17 @@ class New {
   Latest latest;
   Currencies currencies;
   Historical historical;
+  TimeSeries timeseries;
+  Convert convert;
+  Ohlc ohlc;
+  Usage usage;
   New(String app_id) {
     latest = new Latest(app_id);
     historical = new Historical(app_id);
+    timeseries = new TimeSeries(app_id);
+    convert = new Convert(app_id);
+    ohlc = new Ohlc(app_id);
+    usage = new Usage(app_id);
     currencies = new Currencies();
   }
 }
